daf9d1f5bc9ff87e8d2870ff9c036f1960fa3aac
dianafa <diana.falkowska@gmail.com>
2014-09-24 12:34:42 +0200

    CRUD clothes shoes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#see more details in your logs
+ActiveRecord::Base.logger.level = Logger::DEBUG
